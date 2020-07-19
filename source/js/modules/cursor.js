5b2b7c22124487067e39881a7669010e9ebf32a4
Anna P <amltrp@mail.ru>
2020-07-19 23:46:36 +0500

    add cursor

---
+++ source/js/modules/cursor.js
@@ -0,0 +1,39 @@
+'use strict';
+(function () {
+  var cursor = document.querySelector('.cursor');
+
+  if (!cursor) {
+    return;
+  }
+
+  var setCursorPosition = function (evt) {
+    var xPosition = evt.clientX - cursor.clientWidth / 2 + 'px';
+    var yPosition = evt.clientY - cursor.clientHeight / 2 + 'px';
+    cursor.style.transform =
+      'translate(' + xPosition + ',' + yPosition + ')';
+    return {
+      x: xPosition,
+      y: yPosition
+    };
+  };
+
+  document.addEventListener('mousemove', function (evt) {
+    setCursorPosition(evt);
+  });
+
+  var slides = document.querySelectorAll('.js-slider');
+
+  Array.prototype.forEach.call(slides, function (slide) {
+    slide.addEventListener('mouseenter', function (evt) {
+      setCursorPosition(evt);
+      cursor.classList.add('active');
+    });
+    slide.addEventListener('mouseleave', function (evt) {
+      setCursorPosition(evt);
+      if (cursor.classList.contains('active')) {
+        cursor.classList.remove('active');
+      }
+    });
+  });
+
+})();
